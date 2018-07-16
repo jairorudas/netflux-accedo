f33df54d965f0326d4449b22a0417a518936f4f7
jairorudas <jairorudasp@gmail.com>
2018-07-16 19:14:11 -0300

    Configurated color for vuetify

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
      ** Headers of the page
      */
     head: {
-        title: 'netflux-accedo',
+        title: 'Netflux-Accedo',
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -21,6 +21,20 @@ module.exports = {
      ** Build configuration
      */
     mode: 'spa',
+    modules: [
+        '@nuxtjs/vuetify'
+    ],
+    vuetify: {
+        theme: {
+            primary: '#D90429',
+            secondary: '#2B2D42',
+            accent: '#EF233C',
+            error: '#FF5252',
+            info: '#2196F3',
+            success: '#4CAF50',
+            warning: '#FFC107'
+        }
+    },
     build: {
         /*
          ** Run ESLint on save
