a74c945c487e9d1a45fbe9e2f1e49326a75e6f74
jairorudas <jairorudasp@gmail.com>
2018-07-16 18:16:46 -0300

    Nuxt file config added and css array is empty.

---
+++ nuxt.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    /*
+     ** Headers of the page
+     */
+    head: {
+        title: 'netflux-accedo',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: 'Test for a job' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+    /*
+     ** Customize the progress bar color
+     */
+    loading: { color: '#3B8070' },
+    /*
+     ** Build configuration
+     */
+    mode: 'spa',
+    build: {
+        /*
+         ** Run ESLint on save
+         */
+        extend(config, { isDev, isClient }) {
+            if (isDev && isClient) {
+                config.module.rules.push({
+                    enforce: 'pre',
+                    test: /\.(js|vue)$/,
+                    loader: 'eslint-loader',
+                    exclude: /(node_modules)/
+                })
+            }
+        }
+    }
+}
