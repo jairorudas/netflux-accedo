f919df14246f69c59a0d443bcb19848468d1d4ba
jairorudas <jairorudasp@gmail.com>
2018-07-19 07:54:36 -0300

    watch view ready

---
+++ pages/watch.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-content>
+        <v-container fill-height fluid>
+        <v-layout justify-center >
+            <v-flex shrink v-bind:style="{ position: 'relative' }"> 
+                 <v-btn color="#D90429" :nuxt="true" :absolute="true" to="/" :right="true">Back</v-btn>
+                 <h1>   {{ video.title }} </h1> 
+                 <video :src="this.video.contents[0].url" controls autoplay="true" @ended="onPlayerEnded()"></video>
+            </v-flex>
+        </v-layout>
+    </v-container>
+    </v-content>
+   
+</template>
+
+<script>
+export default {
+    
+  data() {
+    return {
+      video: {}
+    };
+  },
+    
+  methods: {
+      
+    onPlayerEnded(player) {
+        this.$router.go(-1)
+        console.log('player ended!', player)
+    },
+  },
+  created() {
+    this.video = JSON.parse(this.$ls.get("movieSelected"));
+    console.log(this.video);
+  }
+};
+</script>
+
+<style scoped>
+</style>
